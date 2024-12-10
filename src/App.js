9f1047a566b74463cf025c3c82e2af7fc9b53e47
Nilesh <nilesh.mishra@hexaviewtech.com>
2024-12-10 15:16:58 +0530

    changes added for seperate article sections

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./index.css";
 import Footer from "./Footer";
 import Headerwithsearch from "./Body/Headerwithsearch";
 import TrendingStories from "./Body/Trendingstories";
-
+import Article from "./Body/Article";
 const App = () => {
   return (
 
@@ -12,48 +12,8 @@ const App = () => {
       < Headerwithsearch />
       <hr className="divider" />
       < TrendingStories />
-
       {/* Categories and Articles */}
-      <section>
-        <h2 className="text-center mb-4">Categories and Articles</h2>
-        <div className="row">
-          {[
-            {
-              title: "City Architecture",
-              description:
-                "Explore the latest trends in urban architecture.",
-              image: "https://picsum.photos/600/600?random=4",
-            },
-            {
-              title: "Home Decor Tips",
-              description:
-                "Discover how to create a warm and inviting home.",
-              image: "https://picsum.photos/600/600?random=5",
-            },
-            {
-              title: "Adventure Awaits",
-              description:
-                "Join our adventure club for thrilling experiences.",
-              image: "https://picsum.photos/600/600?random=6",
-            },
-          ].map((category, index) => (
-            <div className="col-md-4 mb-4" key={index}>
-              <div className="card">
-                <img
-                  src={category.image}
-                  className="card-img-top"
-                  alt={category.title}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{category.title}</h5>
-                  <p className="card-text">{category.description}</p>
-                  <button className="btn btn-primary">Read More</button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
+      <Article/ >
       <Footer />
     </div>
   );
---
+++ src/Body/Article.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Article = () => {
+    return (
+        <>
+            <section>
+                <h2 className="text-center mb-4">Categories and Articles</h2>
+                <div className="row">
+                    {[
+                        {
+                            title: "City Architecture",
+                            description:
+                                "Explore the latest trends in urban architecture.",
+                            image: "https://picsum.photos/600/600?random=4",
+                        },
+                        {
+                            title: "Home Decor Tips",
+                            description:
+                                "Discover how to create a warm and inviting home.",
+                            image: "https://picsum.photos/600/600?random=5",
+                        },
+                        {
+                            title: "Adventure Awaits",
+                            description:
+                                "Join our adventure club for thrilling experiences.",
+                            image: "https://picsum.photos/600/600?random=6",
+                        },
+                    ].map((category, index) => (
+                        <div className="col-md-4 mb-4" key={index}>
+                            <div className="card">
+                                <img
+                                    src={category.image}
+                                    className="card-img-top"
+                                    alt={category.title}
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">{category.title}</h5>
+                                    <p className="card-text">{category.description}</p>
+                                    <button className="btn btn-primary">Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Article;
